59290879730d6dda8d2417953a4e679350212f2c
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-10-11 20:54:08 +0330

    clothes_shop_ui

---
+++ lib/tools/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xffff7066);
+
+const greyColor =  Color(0xFFE0E0E0);
+final greyColor2 =  Colors.grey.shade200;
+
+const darkColor = Color(0xFF212121);
+const whiteColor = Color(0xFFffffff);
