bbe8f8ebdac25a486e6aabdd69134cf1ab36b224
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-10-14 11:45:04 +0330

    clothes_shop_ui

---
+++ lib/tools/styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+const textStyle1=TextStyle(color: darkColor,fontSize:24,fontWeight: FontWeight.bold, );
+const textStyle2=TextStyle(color: darkColor,fontSize:22,fontWeight: FontWeight.bold, );
+const textStyle3=TextStyle(color: darkColor,fontSize:18,fontWeight: FontWeight.bold, );
+const textStyle4=TextStyle(color: darkColor,fontSize:18,fontWeight: FontWeight.normal, );
+const textStyle5=TextStyle(color: darkColor,fontSize:16,fontWeight: FontWeight.bold, );
+const textStyle6=TextStyle(color: darkColor,fontSize:14,fontWeight: FontWeight.bold, );
+//
+final textStyle7=TextStyle(color: Colors.grey.shade600,fontSize:16,fontWeight: FontWeight.bold, );
+final textStyle8=TextStyle(color: Colors.grey.shade600,fontSize:14,fontWeight: FontWeight.bold, );
+final textStyle9=TextStyle(color: Colors.grey.shade600,fontSize:12,fontWeight: FontWeight.bold, );
+//
+const textStyle10=TextStyle(color: primaryColor,fontSize:22,fontWeight: FontWeight.bold, );
+const textStyle11=TextStyle(color: primaryColor,fontSize:18,fontWeight: FontWeight.bold, );
+const textStyle12=TextStyle(color: primaryColor,fontSize:16,fontWeight: FontWeight.bold, );
+//
+const textStyle13=TextStyle(color: whiteColor,fontSize:18,fontWeight: FontWeight.bold,wordSpacing: 1 ,);
+const textStyle14=TextStyle(color: whiteColor,fontSize:16,fontWeight: FontWeight.bold,);
